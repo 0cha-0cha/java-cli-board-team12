5ab2391119f62b9bc2fa06ea28ee9a425d301d45
0cha0cha <taeju1013@gamil.com>
2026-02-24 16:50:11 +0900

    deleteArticle 클래스(기능) 기능 구현

--- src/function/deleteArticle.java
+++ src/function/deleteArticle.java
@@ -1,5 +1,20 @@
 package function;
 
+import java.util.List;
+import data.Article;
+
+// 게시글 삭제 메소드 (손주현)
 public class deleteArticle {
-    
+    Article article;
+    public deleteArticle(Article article) {
+        this.article = article;
+    }
+
+    // 게시글 id를 받으면 해당 게시글을 리스트에서 삭제함
+    // 변경사항: 매개변수 List<Article> articles을 추가하여, 삭제할 게시글이 포함된 리스트를 전달받도록 수정
+    public void delete(List<Article> articles, int id){
+        if(article.getId() == id) {
+            articles.remove(article);
+        }
+    }
 }
