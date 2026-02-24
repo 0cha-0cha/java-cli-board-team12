6faad27cfd67e7477c8754ca6aa9b51e26a3bd67
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2026-02-24 09:40:31 +0900

    feat: 게시글 정보 관리를 위한 Article 클래스 구현

--- src/data/Article.java
+++ src/data/Article.java
@@ -1,5 +1,55 @@
 package data;
 
-public class Article{
-    
+import java.time.LocalDateTime;
+
+public class Article {
+    private int id = 0;
+    private String title;
+    private String content;
+    private String writer;
+    private final LocalDateTime createdAt;
+    private int viewCount;
+
+    public Article(int Id, String title, String content, String writer) {
+        setId(Id);
+        setTitle(title);
+        setContent(content);
+        setWriter(writer);
+        this.createdAt = LocalDateTime.now();  //생성 시 createdAt 자동 세팅
+        this.viewCount = 0; //viewCount 0 시작
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getWriter() {
+        return writer;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public int getViewCount() {
+        return viewCount;
+    }
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
 }
