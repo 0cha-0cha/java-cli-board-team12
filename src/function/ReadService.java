39d319a49aac9facd11976f10aaf218b7a0f1ebe
Jimin Pyo <dkwlrhdwn20@gmail.com>
2026-02-26 16:59:03 +0900

    ReadService

--- src/function/ReadService.java
+++ src/function/ReadService.java
@@ -1,5 +1,6 @@
 package function;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,17 +19,31 @@ public class ReadService implements Page {
 	// 상세 보기 할 게시글 ID 고르기
 	public void select() {
 		// TODO Auto-generated method stub
-		System.out.println("조회를 원하는 게시글 번호를 입력하십시오.");
-        System.out.print("선택 > ");
-        int id = sc.nextInt();
-        
-        Article target = findById(articles, id);
-        if(target == null) {
-        	System.out.println("해당하는 게시글이 없습니다.");
-        }else {
-        	show(id);
-        }
+		Boolean isValid = false;
+		while(!isValid) {
+			System.out.println("조회를 원하는 게시글 번호를 입력하십시오.");
+	        System.out.print("선택 > ");
+	    	try {
+	        	int id = sc.nextInt();
+	        	
+	        	Article target = findById(articles, id);
+	            if(target == null) {
+	            	System.out.println("해당하는 게시글이 없습니다.");
+	            }else {
+	            	show(id);
+	            	isValid = true;
+	            }
+	        // 숫자 이외의 것을 입력했을 경우 다시 입력받기
+	        }catch (InputMismatchException e){	
+            	System.out.println("=================================");
+            	System.out.println("입력이 올바르지 않습니다. 다시 입력해주세요. ");
+            	System.out.println("=================================");   	
+            	// 버퍼 비워 주기
+            	sc.nextLine();
+	        }
+	    }
 	}
+
 
 	@Override
 	// 상세 보기 게시글 보여주기
@@ -49,8 +64,24 @@ public class ReadService implements Page {
 	}
 
 	// 모든 게시글을 반환하는 로직
-	public List<Article> findAll(List<Article> articles){
-		return articles;
+	public void selectAll() {
+		if(articles.isEmpty()) {
+			System.out.println("등록된 게시글이 없습니다. ");
+			return;
+		}
+		
+		System.out.println("<<<<<< 전체 게시글 목록 >>>>>>");
+		System.out.println("---------------------------------");
+        System.out.println("번호 | 제목\t | 작성자 | 작성일 | 조회수 ");
+        System.out.println("---------------------------------");
+		
+		for(Article article : articles) {
+			System.out.print(article.getId() + " | ");
+	        System.out.print(article.getTitle() + " | ");
+	        System.out.print(article.getWriter() + " | ");
+	        System.out.print(article.getCreatedAt() + " | ");
+	        System.out.print(article.getViewCount());
+		}
 	}
 	
 	// 특정 ID로 게시글을 찾아 반환하는 로직
